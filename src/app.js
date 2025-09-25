22381320dc3d98555d180c5cacf02cc1de59ad02
Paresh Kumar Pradhan <pareshkupradhan789@gmail.com>
2025-09-25 11:15:31 +0530

    All setup configured

--- src/app.js
+++ src/app.js
@@ -24,5 +24,14 @@ app.use(express.static("public"));
 // Parse cookies attached to incoming requests
 app.use(cookieParser());
 
+
+
+// routes import
+import userRouter from './routes/user.routes.js'
+
+//routes declaraction
+app.use("/api/v1/users", userRouter)
+// http://localhost:8000/api/v1/users/register
+
 // Export the configured Express app for use in other modules
 export { app };
